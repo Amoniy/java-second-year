abdad3c5ee4abe190482939734b7a2f1fbfdb32f
Anton Kopitsa <anton17.04.1998@gmail.com>
2018-03-28 00:01:31 +0300

    Fix - maximum()

--- java/ru/ifmo/rain/kopitsa/concurrent/IterativeParallelism.java
+++ java/ru/ifmo/rain/kopitsa/concurrent/IterativeParallelism.java
@@ -8,8 +8,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
-import static java.util.stream.Collectors.toList;
-
 public class IterativeParallelism implements ScalarIP {
 
     @Override
@@ -19,16 +17,28 @@ public class IterativeParallelism implements ScalarIP {
         }
         threads = Math.min(threads, values.size());
         List<Thread> threadList = new ArrayList<>();
+
+        ArrayList<T> intermediateResults = new ArrayList<>(threads);
         for (int i = 0; i < threads; i++) {
-            threadList.add(new Thread(String.valueOf(i)));
+            intermediateResults.add(null);
         }
+
         int finalThreads = threads;
-        return Collections.max(
-                threadList.stream().map(thread -> Collections.max(values.subList(
-                        Integer.parseInt(thread.getName()) * values.size() / finalThreads,
-                        (Integer.parseInt(thread.getName()) + 1) * values.size() / finalThreads)
-                        , comparator)).collect(toList())
-                , comparator);
+        for (int i = 0; i < threads; i++) {
+            // Коля, а как вообще так, что это эффективли файнл?
+            int finalI = i;
+            threadList.add(new Thread(() -> {
+                intermediateResults.set(finalI, Collections.max(values.subList(
+                        finalI * values.size() / finalThreads,
+                        (finalI + 1) * values.size() / finalThreads), comparator));
+            }));
+            threadList.get(i).run();
+        }
+        for (int i = 0; i < threads; i++) {
+            threadList.get(i).join();
+        }
+
+        return Collections.max(intermediateResults, comparator);
     }
 
     @Override
