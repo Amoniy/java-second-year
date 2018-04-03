5a3848c8e59b082e71534142c932f1e0bc56c823
Anton Kopitsa <anton17.04.1998@gmail.com>
2018-04-03 22:03:52 +0300

    Rft -copy-paste removed

--- java/ru/ifmo/rain/kopitsa/concurrent/IterativeParallelism.java
+++ java/ru/ifmo/rain/kopitsa/concurrent/IterativeParallelism.java
@@ -3,42 +3,18 @@ package ru.ifmo.rain.kopitsa.concurrent;
 import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class IterativeParallelism implements ScalarIP {
 
     @Override
     public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        if (values.size() == 0) {
-            return null;
-        }
-        threads = Math.min(threads, values.size());
-        Thread[] threadList = new Thread[threads];
-
-        List<T> intermediateResults = new ArrayList<>(threads);
-        for (int i = 0; i < threads; i++) {
-            intermediateResults.add(null);
-        }
-
-        int finalThreads = threads;
-        for (int i = 0; i < threads; i++) {
-            // Коля, а как вообще так, что это эффективли файнл?
-            int finalI = i;
-            threadList[i] = new Thread(() -> {
-                intermediateResults.set(finalI, values.subList(
-                        finalI * values.size() / finalThreads,
-                        (finalI + 1) * values.size() / finalThreads).stream().max(comparator).orElse(null));
-            });
-            threadList[i].start();
-        }
-        for (int i = 0; i < threads; i++) {
-            threadList[i].join();
-        }
-
-        return intermediateResults.stream().max(comparator).orElse(null);
+        List<T> ans = applyFunctionConcurrently(threads, values, listTFunction -> listTFunction.stream().max(comparator).orElse(null));
+        assert ans != null;
+        return ans.stream().max(comparator).orElse(null);
     }
 
     @Override
@@ -48,31 +24,44 @@ public class IterativeParallelism implements ScalarIP {
 
     @Override
     public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        if (values.size() == 0) {
-            return true;
-        }
-        threads = Math.min(threads, values.size());
-
-        Boolean[] intermediateResults = new Boolean[threads];
-        Thread[] threadList = new Thread[threads];
-        int finalThreads = threads;
-        for (int i = 0; i < threads; i++) {
-            int finalI = i;
-            threadList[i] = new Thread(() -> {
-                intermediateResults[finalI] = values.subList(
-                        finalI * values.size() / finalThreads,
-                        (finalI + 1) * values.size() / finalThreads).stream().allMatch(predicate);
-            });
-            threadList[i].run();
-        }
-        for (int i = 0; i < threads; i++) {
-            threadList[i].start();
-        }
-        return Arrays.stream(intermediateResults).allMatch(res -> res);
+        List<Boolean> ans = applyFunctionConcurrently(threads, values, listBooleanFunction -> listBooleanFunction.stream().allMatch(predicate));
+        assert ans != null;
+        return ans.stream().allMatch(intermediateResult -> intermediateResult);
     }
 
     @Override
     public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
         return !all(threads, values, predicate.negate());
+    }
+
+    private <T, R> List<R> applyFunctionConcurrently(int threads, List<? extends T> values, Function<List<? extends T>, R> function) throws InterruptedException {
+        if (values.size() == 0) {
+            return null;
+        }
+        threads = Math.min(threads, values.size());
+        Thread[] threadList = new Thread[threads];
+
+        List<R> intermediateResults = new ArrayList<>(threads);
+        for (int i = 0; i < threads; i++) {
+            intermediateResults.add(null);
+        }
+
+        int finalThreads = threads;
+        for (int i = 0; i < threads; i++) {
+            // Коля, а как вообще так, что это эффективли файнл?
+            int finalI = i;
+            threadList[i] = new Thread(() -> {
+                intermediateResults.set(finalI,
+                        function.apply(values.subList(
+                                finalI * values.size() / finalThreads,
+                                (finalI + 1) * values.size() / finalThreads)));
+            });
+            threadList[i].start();
+        }
+        for (int i = 0; i < threads; i++) {
+            threadList[i].join();
+        }
+
+        return intermediateResults;
     }
 }
